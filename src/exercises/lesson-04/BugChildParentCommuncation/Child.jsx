36954e0e1b38f16620f347f6e38c018271b196a4
Gumaro P <167045860+gpm209@users.noreply.github.com>
2026-04-28 18:59:33 -0700

    All bugs and incomplete code fixed.

--- src/exercises/lesson-04/BugChildParentCommuncation/Child.jsx
+++ src/exercises/lesson-04/BugChildParentCommuncation/Child.jsx
@@ -1,3 +1,5 @@
-export default function Child() {
-  return <button onClick={() => {}}>Increment Counter</button>;
+export default function Child({ increment }) {
+  return <button onClick={increment}>Increment Counter</button>;
 }
+
+/* The Child component accepts the increment function through props from the Parent component and then use it in the onClick handler. This now allows the Child to update the Parent's state */
--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,13 +1,19 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+import { useRef } from 'react';
+
 export default function FillRefFocus() {
-  function focusInput() {}
+  const refInput = useRef(null);
+
+  function focusInput() {
+    refInput.current.focus();
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
 
-      <input type="text" placeholder="Type here..." />
+      <input ref={refInput} type="text" placeholder="Type here..." />
 
       <button onClick={focusInput}>Focus Input</button>
     </div>
