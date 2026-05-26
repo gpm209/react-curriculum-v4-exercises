63cb5220c9feef6bf6cccd0ca07bab10e2034141
Gumaro P <167045860+gpm209@users.noreply.github.com>
2026-05-25 23:41:56 -0700

    Fetching a list of posts and a single post by ID completed

--- src/exercises/lesson-07/FetchOnClick.jsx
+++ src/exercises/lesson-07/FetchOnClick.jsx
@@ -1,13 +1,29 @@
 import './Lesson07Styles.css';
+import { getSinglePost } from './api.js';
+import { useState } from 'react';
 
 export default function FetchOnClick() {
+  const [post, setPost] = useState(null);
+
+  function handleClick() {
+    getSinglePost(1).then((data) => {
+      setPost(data);
+    });
+  }
+
   return (
     <div className="root">
       <h1 className="heading">Fetch single post on click</h1>
-      <button type="button">Get post</button>
+      <button type="button" onClick={handleClick}>
+        Get post
+      </button>
       <div className="content">
-        TODO: Replace me with fetched data when the <code>Get post</code> button
-        is clicked
+        {post && (
+          <div>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        )}
       </div>
     </div>
   );
