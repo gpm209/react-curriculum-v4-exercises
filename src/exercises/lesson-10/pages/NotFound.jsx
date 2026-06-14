6318095289e107aec44e4423a4cb6b03e6c27d54
Gumaro P <167045860+gpm209@users.noreply.github.com>
2026-06-14 16:57:08 -0700

    react routing and navigation implemented

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,16 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const location = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>
+        No page found for: <code>{location.pathname}</code>
+      </p>
+
+      <Link to="/">Go Home</Link>
     </section>
   );
 }
